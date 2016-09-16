1a17b160a42e7d0071de31c5cce3d6394e3354c8
Carlos Rosales <carlos.rosales.fernandez@gmail.com>
2016-09-16 12:53:52 -0500

    Added core build including dgemm_single, p2p_multitask, latency  and stream. Using base 2 rather than base 10 for MPI BW.

---
+++ src/blas/blas_dgemm_single.c
@@ -0,0 +1,142 @@
+/*==============================================================================
+ * Program  : blas_dgemm_single
+ * Revision : 1.6 (2016-09-16)
+ * Author   : Carlos Rosales Fernandez [carlos.rosales.fernandez(at)gmail.com]
+ *==============================================================================
+ * Copyright 2014 Carlos Rosales Fernandez and The University of Texas at Austin
+ *
+ * This code was originally written with support from the National Science 
+ * Foundation under Grant #OCI-1134872
+ *
+ * This file is part of the Performance Assessment Workbench (PAW).
+ * PAW is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU GPL version 2 or (at your option) any later version.
+ *
+ * PAW is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * PAW, in the file COPYING.txt. If not, see <http://www.gnu.org/licenses/>.
+ *==============================================================================
+ * Test sustained double precision matrix matrix multiplication performance 
+ * using the BLAS DGEMM call for a single matrix size. Matrix size can be 
+ * controled using the environmental variable MAX_BLAS_SIZE
+ *============================================================================*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <math.h>
+#include "aux.h"
+#include "constants.h"
+
+#ifdef MKL
+    #include <mkl.h>
+    #include <mkl_cblas.h>
+#else
+    #include <malloc.h>
+    #include <cblas.h>
+#endif
+
+int main(int argc, char **argv)
+{
+    FILE   *fp, *fp2;
+    char   testName[32] = "DGEMM", file1[64], file2[64];
+    unsigned int i, j, size, localSize, nthreads;
+    unsigned int NLOOP = NLOOP_MIN;
+    unsigned int smed = MED_BLAS_SIZE, smax = MAX_BLAS_SIZE;
+    double tScale = SEC, fpScale = GFLOP;
+    double tStart, timeMin, overhead, threshold_lo, threshold_hi;
+    double ops, matSize, usedMem, localMax;
+    double tElapsed[NREPS];
+
+    unsigned long NN;
+    double alpha, beta;
+    double *A, *B, *C;
+
+    // Check for user defined limits
+    checkEnvBLAS( &NLOOP, &smin, &smed, &smax );
+
+    // Initialize variables
+    localMax = 0.0;
+    alpha    = 1.0E0;
+    beta     = 1.0E0;
+    NN       = smax*smax;
+    usedMem  = (double)smax*(double)smax*sizeof(double)*3.0;
+
+    // Allocate and initialize arrays
+    srand( SEED );
+    A = doubleVector( NN );
+    B = doubleVector( NN );
+    C = doubleVector( NN );
+
+    // Check timer overhead in seconds
+    timerTest( &overhead, &threshold_lo, &threshold_hi );
+    // Check how many threads are going to be used
+    nthreads = threadCount();
+    // Open output files and write headers
+    sprintf( file1, "dgemm_single_time-np_%.4d.dat", nthreads );
+    sprintf( file2, "dgemm_single_fp-np_%.4d.dat",   nthreads  );
+    fp  = fopen( file1, "a" );
+    fp2 = fopen( file2, "a" );
+    printHeaders( fp, fp2, testName, usedMem, overhead, threshold_lo );
+
+    //================================================================
+    // Single loop with minimum size to verify that inner loop length  
+    // is long enough for the timings to be accurate                     
+    //================================================================
+    // Warmup processor with a medium size DGEMM
+    cblas_dgemm( CblasRowMajor, CblasNoTrans, CblasNoTrans, smed, smed,
+                 smed, alpha, A, smed, B, smed, beta, C, smed );
+    // Test if current NLOOP is enough to capture fastest test cases
+    tStart = benchTimer();
+    for(j = 0; j < NLOOP; j++){
+         cblas_dgemm( CblasRowMajor, CblasNoTrans, CblasNoTrans, smax, 
+                      smax, smax, alpha, A, smax, B, smax, 
+                      beta, C, smin );
+    }
+    timeMin = benchTimer() - tStart;
+    resetInnerLoop( timeMin, threshold_lo, &NLOOP );
+
+    //================================================================
+    // Execute test for each requested size                  
+    //================================================================
+    // Skip warmup, since it was done when testing the timer
+    // Repeat NREP times to get single node statistics
+    // Call DGEMM to solve C = alpha*(A*B) + beta*C in each processor
+    //================================================================
+    for( i = 0; i < NREPS; i++){
+            tStart = benchTimer();
+            for(j = 0; j < NLOOP; j++){
+                cblas_dgemm( CblasRowMajor, CblasNoTrans, CblasNoTrans, 
+                             size, size, size, alpha, A, size, B, size, 
+                             beta, C, size );
+            }
+            tElapsed[i] = benchTimer() - tStart;
+    }
+
+    // Get time and flops per DGEMM call
+    // ops is total floating point operations per matrix matrix product
+    // matSize is just the leading size of the matrices used
+    ops     = (double)size*(double)size*2.0E0*( (double)size + 1.0E0 );
+    matSize = (double)size;
+    post_process( fp, fp2, threshold_hi, tElapsed, tScale, fpScale, size,
+                   matSize, ops, &NLOOP, &localMax, &localSize );
+    }
+    // Print completion message                 
+    printSummary( fp2, testName, localMax, localSize );
+    fclose( fp2 ); 
+    fclose( fp );
+
+    // Free all allocated memory
+    #ifdef MKL
+        mkl_free( A ); mkl_free( B ); mkl_free( C );
+    #else
+        free( A ); free( B ); free( C );
+    #endif
+
+    return 0;
+}
---
+++ src/mpi/p2p_multitask.c
@@ -0,0 +1,172 @@
+/*==============================================================================
+ * Program  : p2p_multitask
+ * Revision : 1.6 (2016-09-16)
+ * Author   : Carlos Rosales Fernandez [carlos.rosales.fernandez(at)gmail.com]
+ *==============================================================================
+ * Copyright 2014 Carlos Rosales Fernandez and The University of Texas at Austin
+ *
+ * This code was originally written with support from the National Science 
+ * Foundation under Grant #OCI-1134872
+ *
+ * This file is part of the Performance Assessment Workbench (PAW).
+ * PAW is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU GPL version 2 or (at your option) any later version.
+ *
+ * PAW is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * PAW, in the file COPYING.txt. If not, see <http://www.gnu.org/licenses/>.
+ *==============================================================================
+ * Test sustained MPI bandwidth using a ping-ping setup with non-blocking 
+ * MPI_Isend/MPI_Irecv message pairs completed by MPI_Wait calls. This is an 
+ * attempt at measuring the maximum sustained one-directional bandwidth of
+ * the system using a single test size.
+ *============================================================================*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <mpi.h>
+#include "aux.h"
+#include "aux_mpi.h"
+#include "constants.h"
+
+int main(int argc, char **argv)
+{
+    FILE   *fp, *fp2;
+    char   testName[32] = "MPI_Multitask", file1[64], file2[64];
+    int    dblSize, proc, nprocs, npairs, partner, tag = 0;
+    unsigned int i, j, size, localSize, NLOOP = NLOOP_MIN;
+    unsigned int smed = MED_P2P_SIZE, smax = MAX_P2P_SIZE;
+    double tScale = USEC, bwScale = MB_8;
+    double tStart, timeMin, timeMinGlobal, overhead, threshold_lo, threshold_hi;
+    double msgBytes, sizeBytes, localMax, UsedMem;
+    double tElapsed[NREPS], tElapsedGlobal[NREPS];
+    double *A, *B;
+    MPI_Status  stat1, stat2;
+    MPI_Request req1,  req2;
+
+
+    // Initialize parallel environment
+    MPI_Init( &argc, &argv );
+    MPI_Comm_size( MPI_COMM_WORLD, &nprocs );
+    MPI_Comm_rank( MPI_COMM_WORLD, &proc );
+
+    // Test input parameters
+    if( nprocs%2 != 0 && proc == 0 )
+        fatalError( "P2P test requires an even number of processors" );
+
+    // Check for user defined limits
+    checkEnvP2P( proc, &NLOOP, &smin, &smed, &smax );
+
+    // Initialize local variables
+    localMax = 0.0;
+    dblSize  = sizeof(double);
+    npairs   = nprocs/2;
+    if( proc < npairs  ) partner = proc + npairs;
+    if( proc >= npairs ) partner = proc - npairs;
+    UsedMem = (double)smax*(double)dblSize*2.0;
+
+    // Allocate and initialize arrays
+    srand( SEED );
+    A  = doubleVector( smax );
+    B  = doubleVector( smax );
+
+    // Open output file and write header
+    if( proc == 0 ){
+        // Check timer overhead in seconds
+        timerTest( &overhead, &threshold_lo, &threshold_hi );
+        // Open output files and write headers
+        sprintf( file1, "multitask_time-np_%.4d.dat", nprocs );
+        sprintf( file2, "multitask_bw-np_%.4d.dat",   nprocs );
+        fp  = fopen( file1, "a" );
+        fp2 = fopen( file2, "a" );
+        printHeaders( fp, fp2, testName, UsedMem, overhead, threshold_lo );
+    }
+
+    //================================================================
+    // Single loop with minimum size to verify that inner loop length  
+    // is long enough for the timings to be accurate                     
+    //================================================================
+    // Warmup with a medium size message
+    if( proc < npairs ){
+        MPI_Isend( A, smed, MPI_DOUBLE, partner, tag, MPI_COMM_WORLD, &req1 );
+        MPI_Wait( &req1, &stat1 );
+    }else{
+        MPI_Irecv( B, smed, MPI_DOUBLE, partner, tag, MPI_COMM_WORLD, &req2);
+        MPI_Wait( &req2, &stat2);
+    }
+    // Test if current NLOOP is enough to capture the test cases
+    MPI_Barrier( MPI_COMM_WORLD );
+    tStart = benchTimer();
+    if( proc < npairs ){
+        for(j = 0; j < NLOOP; j++){
+            MPI_Isend( A, smax, MPI_DOUBLE, partner, tag, MPI_COMM_WORLD, &req1 );
+            MPI_Wait( &req1, &stat1 );
+        }
+    }else{
+        for(j = 0; j < NLOOP; j++){
+            MPI_Irecv( B, smax, MPI_DOUBLE, partner, tag, MPI_COMM_WORLD, &req2);
+            MPI_Wait( &req2, &stat2 );
+        }
+    }
+    timeMin = benchTimer() - tStart;
+    MPI_Reduce( &timeMin, &timeMinGlobal, 1, MPI_DOUBLE, MPI_MIN, 0, MPI_COMM_WORLD );
+    if( proc == 0 ) resetInnerLoop( timeMinGlobal, threshold_lo, &NLOOP );
+    MPI_Bcast( &NLOOP, 1, MPI_INT, 0, MPI_COMM_WORLD );
+
+
+    //================================================================
+    // Execute test               
+    //================================================================
+    // Skip warmup, since it was effectively done in the timer test
+    // Repeat NREPS to collect statistics
+    //================================================================
+    for(i = 0; i < NREPS; i++){
+        MPI_Barrier( MPI_COMM_WORLD );
+        tStart = benchTimer();
+        if( proc < npairs ){
+            for(j = 0; j < NLOOP; j++){
+    	        MPI_Isend( A, size, MPI_DOUBLE, partner, tag, MPI_COMM_WORLD, &req1 );
+                MPI_Wait( &req1, &stat1 );
+            }
+        }
+        else{
+            for(j = 0; j < NLOOP; j++){
+     	        MPI_Irecv( B, size, MPI_DOUBLE, partner, tag, MPI_COMM_WORLD, &req2 );
+                MPI_Wait( &req2, &stat2 );
+            }
+        }
+        tElapsed[i] = benchTimer() - tStart;
+    } 
+    MPI_Reduce( tElapsed, tElapsedGlobal, NREPS, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+    // Only task 0 needs to do the analysis of the collected data
+    if( proc == 0 ){
+        // sizeBytes is size to write to file
+        // msgBytes is actual data exchanged on the wire
+        msgBytes  = (double)size*(double)npairs*(double)dblSize;
+        sizeBytes = (double)size*(double)dblSize;
+        post_process( fp, fp2, threshold_hi, tElapsedGlobal, tScale, 
+                      bwScale, size*dblSize, sizeBytes, msgBytes, &NLOOP, 
+                      &localMax, &localSize );
+    }
+    MPI_Bcast( &NLOOP, 1, MPI_INT, 0, MPI_COMM_WORLD );
+
+    //================================================================
+    // Print completion message, free memory and exit                  
+    //================================================================
+    if( proc == 0 ){
+        printSummary( fp2, testName, localMax, localSize );
+        fclose( fp2 ); 
+        fclose( fp );
+    }
+    free( A );
+    free( B );
+
+    MPI_Finalize();
+    return 0;
+}
+
